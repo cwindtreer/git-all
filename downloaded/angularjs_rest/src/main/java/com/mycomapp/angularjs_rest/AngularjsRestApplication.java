dc2490637afa99fe9eaac51300d3e48d35bf1dfe
cwindtreer <chikoei@gmail.com>
2018-08-15 17:19:27 +0800

    a

---
+++ downloaded/angularjs_rest/src/main/java/com/mycomapp/angularjs_rest/AngularjsRestApplication.java
@@ -0,0 +1,12 @@
+package com.mycomapp.angularjs_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularjsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularjsRestApplication.class, args);
+	}
+}
